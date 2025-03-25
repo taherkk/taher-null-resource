0d1ae038e02ba858d2dbd01b384676a373fc0a12
Taher Kathanawala <28436188+taherkk@users.noreply.github.com>
2025-03-25 16:42:40 +0530

    create s3 bucket

--- main.tf
+++ main.tf
@@ -1,30 +1,21 @@
-# module "custom_git" {
-#  source = "git@github.com:rixhieloomis/aws-terraform.git"
-#  }
-
-variable "resource_count" {
-  default = 1
-}
-
-variable "wait_time" {
-  default = 10
-}
-
-resource "null_resource" "hello_script" {
-  count = var.resource_count
-
-  triggers = {
-    timestamp = timestamp()
-  }
-
-  provisioner "local-exec" {
-    command = <<EOT
-      echo 'Hello, World!'
-      sleep ${var.wait_time}
-    EOT
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
   }
 }
 
-output "message_lengths" {
-  value = [for i in range(var.resource_count): length("Hello, World!")]
+# Configure the AWS Provider
+provider "aws" {
+  region = "eu-central-1"
+}
+
+resource "aws_s3_bucket" "example" {
+  bucket = "taher-test-bucket"
+
+  tags = {
+      Environment = "Test"
+  }
 }
